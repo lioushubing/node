ad290bcb82dc899983ee8db4df49b48490ba0687
lioushubing <lioushuibing@outlook.com>
2018-12-18 15:06:20 +0800

    00

--- 00/appp.js
+++ 00/appp.js
@@ -1,16 +1,13 @@
 const express=require('express')
-const bodyParser=require('body-parser')
+const path=require('path')
 const app=express()
+app.get('/index',(req,res)=>{
+    res.redirect('list')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use((req,res)=>{
-    // let str=req.query
-    // console.log(str);
-    // res.end('ok')
-   // let str=req.body
-    console.log(req.body);
 
-    res.end('ok')
+
 })
 
-app.listen(9999, console.log('http://localhost:9999','服务器已响应'))
+app.listen(9999, ()=>{
+     console.log('http://localhost:9999','服务器已响应')
+})
